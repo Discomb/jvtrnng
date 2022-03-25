733d7868cb4fca75439f5f71bb2d537bbe2e921a
Discomb <lllkoutalll@gmail.com>
2022-03-25 14:14:17 +0300

    Поправлено форматирование, переименованы поля класса Point

--- sandbox/src/main/java/JVtraining.java
+++ sandbox/src/main/java/JVtraining.java
@@ -1,26 +1,24 @@
-public class JVtraining{
+public class JVtraining {
 
+    public static void main(String[] args) {
 
-  
-  public static void main(String[] args) {
+        Point a = new Point(0, -19);
+        Point b = new Point(6, 10);
+        Point c = new Point(75, -56);
 
-    Point a = new Point(0, -19);
-    Point b = new Point(6,10);
-    Point с = new Point(75,-56);
+        System.out.println();
+        System.out.println("==== Программа в рамках домашнего задания ====");
+        System.out.println();
+        System.out.println("==== ДЗ №2 ====");
+        System.out.println("Результат работы функции distance:   Расстояние между точками a и b равно " + distance(a, b));
+        System.out.println("Результат работы метода класса:      Расстояние между точками a и c равно " + Point.distance(c, a));
+        System.out.println("===============");
 
-    System.out.println();
-    System.out.println("==== Программа в рамках домашнего задания ====");
-    System.out.println();
-    System.out.println("==== ДЗ №2 ====");
-    System.out.println("Результат работы функции distance:   Расстояние между точками a и b равно " + distance(a, b));
-    System.out.println("Результат работы метода класса:      Расстояние между точками a и c равно " + Point.distance(с, a));
-    System.out.println("===============");
-  
-  }
+    }
 
-  public static double distance (Point p1, Point p2){
-    return Math.sqrt((Math.pow((p2.p1 - p1.p1), 2) + Math.pow((p2.p2 - p1.p2), 2)));
-  }
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2)));
+    }
 
 
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,15 +1,15 @@
 public class Point {
 
-    public double p1;
-    public double p2;
+    public double x;
+    public double y;
 
     public Point(double x, double y) {
-        this.p1 = x;
-        this.p2 = y;
+        this.x = x;
+        this.y = y;
     }
 
     public static double distance (Point p1, Point p2){
-        return Math.sqrt((Math.pow((p2.p1 - p1.p1), 2) + Math.pow((p2.p2 - p1.p2), 2)));
+        return Math.sqrt((Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2)));
     }
 
 }
